0dbac96d70003573e83db72669cb97fb4eaff4bf
Basic Lady Programmer <84853907+basicladyprogrammer@users.noreply.github.com>
2022-10-29 13:34:51 +0300

    Add files via upload

---
+++ kmain.c
@@ -0,0 +1,13 @@
+#include "frame_buffer.h"
+#include "serial_port.h"
+
+    
+    int main(){
+
+           char ptr2[] = "Welcome to MyOS";
+   
+    
+    fb_write( ptr2, 19);
+    serial_write(0x3F8, ptr2, 19);
+    fb_write(ptr2, 19);
+    }
